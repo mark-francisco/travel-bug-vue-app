1ac6b4d6b0b458821df1268a7c8fe6836f78e846
Mark Francisco <mfrancisco581@gmail.com>
2021-04-16 16:52:25 -0400

    Add working Signup form with Users POST request to back-end

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,19 +1,86 @@
 <template>
   <div class="signup">
-    <h1>{{ message }}</h1>
+    <!-- on submit, prevent the browser from refreshing the page. instead, run the custom submit() method defined below. -->
+    <form v-on:submit.prevent="submit()">
+      <h1>Signup</h1>
+      <ul>
+        <li class="error-messages" v-for="error in errors" v-bind:key="error">
+          {{ error }}
+        </li>
+      </ul>
+      <div class="form-group">
+        <label>First Name:</label>
+        <input type="text" v-model="firstName" />
+      </div>
+      <div class="form-group">
+        <label>Last Name:</label>
+        <input type="text" v-model="lastName" />
+      </div>
+      <div class="form-group">
+        <label>Username:</label>
+        <input type="text" v-model="username" />
+      </div>
+      <div class="form-group">
+        <label>Email:</label>
+        <input type="email" v-model="email" />
+      </div>
+      <div class="form-group">
+        <label>Password:</label>
+        <input type="password" v-model="password" />
+      </div>
+      <div class="form-group">
+        <label>Password Confirmation:</label>
+        <input type="password" v-model="passwordConfirmation" />
+      </div>
+      <input type="submit" value="Sign Up!" />
+    </form>
   </div>
 </template>
 
-<style></style>
+<style>
+.error-messages {
+  color: red;
+}
+</style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "do you wanna sign up? if so, please e-mail credit card # and CVV to mark@example.com",
+      firstName: "",
+      lastName: "",
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: [],
     };
   },
   created: function () {},
-  methods: {},
+  methods: {
+    submit: function () {
+      let params = {
+        // params must follow the Rails variable naming conventions (snake_case)
+        first_name: this.firstName,
+        last_name: this.lastName,
+        username: this.username,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation,
+      };
+      axios
+        .post("/api/users/", params)
+        .then((res) => {
+          console.log(res.data);
+          // you can use the global variable "$router" to reference this Vue app's router (var declared in index.js from "vue-router" library).
+          this.$router.push("/login");
+        })
+        .catch((err) => {
+          this.errors = err.response.data.errors;
+        });
+    },
+  },
 };
 </script>

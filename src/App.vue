45d733e4881a6bb729143d85d5cda7017ce0f7a5
Mark Francisco <mfrancisco581@gmail.com>
2021-04-28 15:09:21 -0500

    Import Ecoland Bootstrap theme

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <span v-if="!isLoggedIn()">
         <router-link to="/">Home</router-link>
         |
@@ -22,33 +22,219 @@
         |
         <router-link to="/logout">Log Out</router-link>
       </span>
-    </div>
+    </div> -->
+
+    <!-- ECOLAND NAVBAR -->
+    <nav
+      class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light site-navbar-target"
+      id="ftco-navbar"
+    >
+      <div class="container">
+        <a class="navbar-brand" href="index.html">Travel Bug</a>
+        <button
+          class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle"
+          type="button"
+          data-toggle="collapse"
+          data-target="#ftco-nav"
+          aria-controls="ftco-nav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="oi oi-menu"></span>
+          Menu
+        </button>
+
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav nav ml-auto">
+            <li class="nav-item">
+              <a href="#home-section" class="nav-link"><span>Home</span></a>
+            </li>
+            <li class="nav-item">
+              <a href="#services-section" class="nav-link"><span>Services</span></a>
+            </li>
+            <li class="nav-item">
+              <a href="#about-section" class="nav-link"><span>About</span></a>
+            </li>
+            <li class="nav-item">
+              <a href="#destination-section" class="nav-link"><span>Destination</span></a>
+            </li>
+            <li class="nav-item">
+              <a href="#hotel-section" class="nav-link"><span>Hotel</span></a>
+            </li>
+            <li class="nav-item">
+              <a href="#restaurant-section" class="nav-link"><span>Restaurant</span></a>
+            </li>
+            <li class="nav-item">
+              <a href="#blog-section" class="nav-link"><span>Blog</span></a>
+            </li>
+            <li class="nav-item">
+              <a href="#contact-section" class="nav-link"><span>Contact</span></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
     <router-view />
+
+    <footer class="ftco-footer ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">
+                About
+                <span><a href="index.html">Travel Bug</a></span>
+              </h2>
+              <p>
+                Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+                blind texts.
+              </p>
+              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+                <li class="ftco-animate">
+                  <a href="#"><span class="icon-twitter"></span></a>
+                </li>
+                <li class="ftco-animate">
+                  <a href="#"><span class="icon-facebook"></span></a>
+                </li>
+                <li class="ftco-animate">
+                  <a href="#"><span class="icon-instagram"></span></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-4">
+              <h2 class="ftco-heading-2">Information</h2>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#">
+                    <span class="icon-long-arrow-right mr-2"></span>
+                    Online Enquiry
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon-long-arrow-right mr-2"></span>
+                    General Enquiry
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon-long-arrow-right mr-2"></span>
+                    Booking
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon-long-arrow-right mr-2"></span>
+                    Privacy
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon-long-arrow-right mr-2"></span>
+                    Refund Policy
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon-long-arrow-right mr-2"></span>
+                    Call Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Experience</h2>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#">
+                    <span class="icon-long-arrow-right mr-2"></span>
+                    Adventure
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon-long-arrow-right mr-2"></span>
+                    Hotel and Restaurant
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon-long-arrow-right mr-2"></span>
+                    Beach
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon-long-arrow-right mr-2"></span>
+                    Nature
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon-long-arrow-right mr-2"></span>
+                    Camping
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="icon-long-arrow-right mr-2"></span>
+                    Party
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Have a Questions?</h2>
+              <div class="block-23 mb-3">
+                <ul>
+                  <li>
+                    <span class="icon icon-map-marker"></span>
+                    <span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span class="icon icon-phone"></span>
+                      <span class="text">+2 392 3929 210</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span class="icon icon-envelope"></span>
+                      <span class="text">info@yourdomain.com</span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <p>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              Copyright &copy;
+              {{ new Date().getFullYear() }}
+              All rights reserved | This template is made with
+              <i class="icon-heart color-danger" aria-hidden="true"></i>
+              by
+              <a href="https://colorlib.com" target="_blank">Colorlib</a>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
 
 <script>
 export default {
